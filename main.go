ba111943a57c918fa7c24b89c8cf260e88fcaa94
christopher-koleda <koleda.christopher@gmail.com>
2020-12-04 15:15:00 -0500

    basic convert to csv seems working

--- main.go
+++ main.go
@@ -1,40 +1,34 @@
 package main
 
 import (
+	"encoding/csv"
 	"log"
+	"os"
 
 	"github.com/tealeg/xlsx/v3"
 )
 
+var (
+	isFirstSuccessCall = false
+	records            = [][]string{
+		{"sku", "tier_price_website", "tier_price_customer_group", "tier_price_qty", "tier_price", "tier_price_value_type"},
+	}
+)
+
 type discountItem struct {
 	sku         string
-	location    string
-	discountVal map[string]string //percent group, amount off
+	discountVal map[string]string //percent group name ex 25%, actual discount amount
 }
 
+// Quick program to convert a companies xsls discount spreadsheet into a magento 2 readible csv for import
 func main() {
 	// open an existing file
 	file, err := xlsx.OpenFile("./data/discount_item_list.xlsx")
-
-	// go through each row,
-	// get first cell (A) this is sku
-	// if any subsequent cell has a value, add that cells discount group to the tranlated xlsx document
 
 	if err != nil {
 		log.Fatalf("FileOpenErr: %+v", err)
 	}
 
-	// file now contains a reference to the workbook
-	// show all the sheets in the workbook
-	// log.Println("Sheets in this file:")
-
-	// for i, sh := range file.Sheets {
-	// 	log.Println(i, sh.Name)
-	// }
-	// log.Println("----")
-
-	// this creates a new file, we might be able to use this to make the magento file
-	// wb := xlsx.NewFile();
 	sh, ok := file.Sheet["Sheet1"]
 
 	if !ok {
@@ -42,16 +36,7 @@ func main() {
 		return
 	}
 
-	// get the coordinates of the cell, if its
-	// 1st in a cell its the sku
-	// 2nd in a cell its 10 %
-	// 3rd in a cell its 20 %
-	// 4th in a cell its 25 %
-	// 5th in a cell its 30 %
-	// 6th in a cell its 35 %
-	// 7th in a cell its 38 %
-	// 9th in a cell its 40 %
-	// make a map for this string to convert
+	// maps cell letters to discount code
 	cellToDiscount := map[string]string{
 		"A": "SKU",
 		"B": "10%",
@@ -63,13 +48,9 @@ func main() {
 		"H": "40%",
 	}
 
-	// row, err := sh.Row(1)
-
-	// let's so something with the row ...
-	// log.Println("Max row is", sh.MaxRow)
-
-	// for each row
-	// we want to get each cell value
+	// for each row in our default sheet we make a discount item
+	// when we hit a sku cell, we add data to our item
+	// when we hit a data sku we add data to our map related to what cell we're in
 	sh.ForEachRow(func(r *xlsx.Row) error {
 		di := &discountItem{}
 		return r.ForEachCell(func(c *xlsx.Cell) error {
@@ -93,27 +74,55 @@ func main() {
 					log.Fatalln(err.Error())
 				}
 
+				// to remove 0 values in general
+				// if (val != 0) then run di.discountVal[code] = val
 				di.discountVal[code] = value
 
-				// if we hit h aka we know we can run our translate function and move on to our next sku
+				// if we hit h aka 40% we know we can run our translate function and move on to our next sku
 				// because this is our final bound aka our final customer group
-				if code == "40%" {
+				if code == "40%" && di.sku != "" {
 					translateRowToMagentoForm(*di)
 				}
 			}
-			// log.Printf("%+v", *di)
 			return err
-
 		})
-
 	})
+
+	// here we create our exported file data directory
+	// and write all our translated data from the xlsx into the csv
+	csvFile, err := os.Create("./data/export_advanced_pricing.csv")
+
+	if err != nil {
+		log.Fatalf("CSVFileCreateErr: %+v", err)
+	}
+
+	defer csvFile.Close()
+
+	// we create a new csvwriter and pass our csv file
+	writer := csv.NewWriter(csvFile)
+	defer writer.Flush()
+
+	// for each slice in records we write it to our csv file
+	for _, record := range records {
+		if err := writer.Write(record); err != nil {
+			log.Fatalln("error writing record to csv:", err)
+		}
+	}
+
+	// check the writer for errors before we close
+	if err := writer.Error(); err != nil {
+		log.Fatalf("CSVWriterHasErrorErr: %+v", err)
+	}
 }
 
+// This function takes a discount item, takes its values and adds it to our records slice
+// which is a csv readible format, which we use to write to csv file at the end of the program
 func translateRowToMagentoForm(di discountItem) {
-	// if all items are 0, don't add to new xlsx file
 	cm := len(di.discountVal)
 	count := 1
 	addFlag := true
+
+	// if all items are 0, don't add to new xlsx file
 	for _, v := range di.discountVal {
 		if v == "0" {
 			count++
@@ -123,11 +132,13 @@ func translateRowToMagentoForm(di discountItem) {
 		}
 	}
 
+	// if all is good, for each item we have in our map we add a row to our eventual converted csv
+	// each map entry represents one row on the csv, and each row on the csv reprsents one item and its price
+	// at a given discount group EX: sku 909, discount group 40% tier price 30 --> this may be the actual percentage
+	// off for certainm items even if they are in 40% group -- weird business logic
 	if addFlag {
-		log.Printf("%+v", di)
-		// create the magento sheet
-
-		// first create a new row with all the required values
-		// then insert each item as a cell in a row
+		for k, v := range di.discountVal {
+			records = append(records, []string{di.sku, "All Websites [USD]", k, "1.000", v, "Discount"})
+		}
 	}
 }
